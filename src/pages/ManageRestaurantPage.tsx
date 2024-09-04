29c1d89f7247f7858acc87957097f77b97b5c531
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-09-04 11:32:15 +0100

    implement restaurant management

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,46 @@
+import {
+    useCreateMyRestaurant,
+    useGetMyRestaurant,
+    
+    useUpdateMyRestaurant,
+  } from "@/api/MyRestaurantApi";
+  
+  import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+  import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+  
+  const ManageRestaurantPage = () => {
+    const { createRestaurant, isLoading: isCreateLoading } =
+      useCreateMyRestaurant();
+    const { restaurant } = useGetMyRestaurant();
+    const { updateRestaurant, isLoading: isUpdateLoading } =
+      useUpdateMyRestaurant();
+   
+
+  
+    const isEditing = !!restaurant;
+  
+    return (
+      <Tabs defaultValue="orders">
+        <TabsList>
+          <TabsTrigger value="orders">Orders</TabsTrigger>
+          <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+        </TabsList>
+        <TabsContent 
+          value="orders"
+          className="space-y-5 bg-gray-50 p-10 rounded-lg"
+        >
+           Order Item Card
+        </TabsContent>
+        <TabsContent value="manage-restaurant">
+          <ManageRestaurantForm
+            restaurant={restaurant}
+            onSave={isEditing ? updateRestaurant : createRestaurant}
+            isLoading={isCreateLoading || isUpdateLoading}
+          />
+        </TabsContent>
+      </Tabs>
+    );
+  };
+  
+  export default ManageRestaurantPage;
+  
